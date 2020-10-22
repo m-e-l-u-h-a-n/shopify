2373a12ac6b57f5109481acd0d7d68c4f7350fe0
m-e-l-u-h-a-n <pururshottam.tiwari.cd.cse19@itbhu.ac.in>
2020-10-22 08:56:33 +0530

    Added Procfile and .env config

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -4,7 +4,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
+SECRET_KEY = os.getenv('SECRET_KEY')
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
@@ -104,4 +104,4 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = '/'
 
 # stripe secret key
-STRIPE_SECRET_KEY = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
