c2d97e5319a23e737d7bca54b7e83586973bd577
m-e-l-u-h-a-n <pururshottam.tiwari.cd.cse19@itbhu.ac.in>
2020-10-24 16:24:37 +0530

    authentication: adds google and facebook signin methods.
    Note: Facebook login method may not work as we use http in development mode.

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -4,7 +4,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
@@ -19,6 +19,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
     # auth (all auth package addition)
     'core',
     # crispy installed app
